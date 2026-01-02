92667e58c9017260bc27e34b02b6abd1658e2846
khaled883 <khaledyoussef883@gmail.com>
2026-01-02 11:23:23 +0200

    adding responsiveness feature

--- lib/screens/calculator_screen.dart
+++ lib/screens/calculator_screen.dart
@@ -77,36 +77,40 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Basic Calculator"),
+      appBar: AppBar(title: const Text("Basic Calculator",
+      style: TextStyle(fontSize: 20),),
       centerTitle: true,),
-      body: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),
-            color: Colors.grey[300],
+      body: Container(
+        height: MediaQuery.of(context).size.height*0.9,
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),
+              color: Colors.grey[300],
+              ),
+              height: MediaQuery.of(context).size.height*0.2,
+              
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                displayText,
+                style: const TextStyle(fontSize: 60),
+              ),
             ),
-            height: MediaQuery.of(context).size.height*0.3,
-            
-            alignment: Alignment.centerRight,
-            padding: const EdgeInsets.all(16),
-            child: Text(
-              displayText,
-              style: const TextStyle(fontSize: 60),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(children: [buildButton('7'), buildButton('8'), buildButton('9'), buildButton('/')]),
+                  Row(children: [buildButton('4'), buildButton('5'), buildButton('6'), buildButton('*')]),
+                  Row(children: [buildButton('1'), buildButton('2'), buildButton('3'), buildButton('-')]),
+                  Row(children: [buildButton('0'), buildButton('C'), buildButton('='), buildButton('+')]),
+                  SizedBox(height: 50,)
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Row(children: [buildButton('7'), buildButton('8'), buildButton('9'), buildButton('/')]),
-                Row(children: [buildButton('4'), buildButton('5'), buildButton('6'), buildButton('*')]),
-                Row(children: [buildButton('1'), buildButton('2'), buildButton('3'), buildButton('-')]),
-                Row(children: [buildButton('0'), buildButton('C'), buildButton('='), buildButton('+')]),
-                SizedBox(height: 50,)
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
